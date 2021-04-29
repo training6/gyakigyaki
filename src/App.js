46e20af1cd5a8f23d6be71934f33c6f6cb03e175
Terez <69874794+training6@users.noreply.github.com>
2021-04-29 20:08:16 +0200

    test

--- src/App.js
+++ src/App.js
@@ -5,9 +5,24 @@ import HamBurgerMenu from '../src/components/HamBurgerMenu';
 function App() {
   return (
     <div className="App">
-      
+      <div className="box">
+      <HamBurgerMenu />
+        <div className="text1">
+          <p>Vásárlásról és kiszállításról</p>
+          <p>Termékek listája</p>
+          <p>Szolgáltatások</p>
+          <p>Kapcsolat</p>
+        </div>
+        <div className="text2">
+          <p>Általásnos szerződési feltételek</p>
+          <p>Adatkezelési nyilatkozat</p>
+          <p>Impresszum</p>
+        </div>
+        <div className='title'>
+          <h3>Morella</h3>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
